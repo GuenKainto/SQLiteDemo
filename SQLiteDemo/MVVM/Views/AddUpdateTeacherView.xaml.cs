c8f2ae8fdd4036c555a145602a2d55ac44edab05
GuenKainto <khanhtho10122002@gmail.com>
2023-10-25 16:29:59 +0700

    Update all View  of Teacher

---
+++ SQLiteDemo/MVVM/Views/AddUpdateTeacherView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SQLiteDemo.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for AddUpdateTeacherView.xaml
+    /// </summary>
+    public partial class AddUpdateTeacherView : Window
+    {
+        public AddUpdateTeacherView()
+        {
+            InitializeComponent();
+        }
+    }
+}
